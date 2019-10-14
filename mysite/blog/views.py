611d0499ad8cf301f4f28d795cb5997f5650f82e
VadimSkasyrskiy <v.skasurskii@gmail.com>
2019-10-14 14:10:00 +0300

    moved to class handler

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
     
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger: # если page не является целым числом
-        posts = paginator.page(1) # возвращаем первую страницу
-    except EmptyPage: # если page больше чем общее кол-во страниц
-        posts = paginator.page(paginator.num_pages) # возвращаем последнюю
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger: # если page не является целым числом
+#         posts = paginator.page(1) # возвращаем первую страницу
+#     except EmptyPage: # если page больше чем общее кол-во страниц
+#         posts = paginator.page(paginator.num_pages) # возвращаем последнюю
         
-    return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+#     return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+    
 
 
 def post_detail(request, year, month, day, post):
